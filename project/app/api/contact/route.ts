10350c4c12cd8438944b2764c882dd8214efc679
M.Patel <Muhammad.Patel.4@city.ac.uk>
2025-03-24 00:33:47 +0000

    test

--- project/app/api/contact/route.ts
+++ project/app/api/contact/route.ts
@@ -1,17 +1,19 @@
 import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
-
-const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
   const { email, subject, message } = await request.json();
+
   try {
+    const { Resend } = await import('resend');
+    const resend = new Resend(process.env.RESEND_API_KEY);
+
     const data = await resend.emails.send({
       from: 'you@yourdomain.com',
       to: email,
       subject,
-      text: message
+      text: message,
     });
+
     return NextResponse.json({ data });
   } catch (error) {
     return NextResponse.json(
